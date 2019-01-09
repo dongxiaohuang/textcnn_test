395cf2465c8c843984f3689be23bb7de95e4a329
huangdongxiao2 <huangdongxiao2@jd.com>
2019-01-09 21:14:39 +0800

    update data preprocessor

--- utils/dataprocessor.py
+++ utils/dataprocessor.py
@@ -1,5 +1,22 @@
 import tensorflow.contrib.keras as kr
+import re
+def data_preporcessor(input_str):
+    # input_str = ['<Number>' if i.isdigit() else i for i in input_str]
+    # alert process
+    input_str = ''.join(input_str)
+    input_str = re.sub("查询|查一下|查询一下|查下|查询下|查","查询", input_str)
+    input_str = re.sub("[0123456789零一二三四五六七八九十]","@", input_str)
+    input_str = re.sub("[。，,.：:]","点", input_str)
+    input_str = re.sub("删除|取消|关闭|删了|删掉|停止","删除", input_str)
+    input_str = re.sub("闹钟|提醒|闹铃|安排|日程","闹钟", input_str)
+    input_str = re.sub("凌晨|黎明|清晨|早上|早晨|上午|中午|下午|晚儿|晚上|傍晚|夜里|半夜|深夜|午夜","时段", input_str)
+    input_str = re.sub("星期|礼拜","星期", input_str)
+    content = list(input_str)
+    content = ['<Number>' if a=='@' else a for a in content]
+    return content
+
 def process_txt(input_str, word_to_id, seq_length):
+    input_str = data_preporcessor(input_str)
     unkIndex = word_to_id.get("<unk>", 1)
     data_id = []
     data_id.append([word_to_id.get(w, unkIndex) for w in input_str])
@@ -36,7 +53,7 @@ def process_file(source_path, word_to_id, seq_length, cat_to_id):
             if len(line.strip().split('\t')) < 2:
                 continue
             content, label = line.strip().split('\t')
-            content = list(content.strip())
+            content = data_preporcessor(content)
             unkIndex = word_to_id.get("<unk>", 1)
             encoded_content = [word_to_id.get(word,unkIndex) for word in content]
             encoded_label = cat_to_id[label.strip()]
